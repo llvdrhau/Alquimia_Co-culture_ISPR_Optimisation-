1251579c3a587ed69369a91c9886bd1d8b1b3b07
Lucas Van der Hauwaert <79929276+llvdrhau@users.noreply.github.com>
2022-08-23 18:34:31 +0200

    Add files via upload
    adding scripts:
     RunMeFirst.m 
     runBootStrapOvernight.m

---
+++ RUN_ME_FIRST.m
@@ -0,0 +1,16 @@
+%% RUN ME FIRST 
+% clears all previous variables and links all folders to the working 
+% directory so all functions can be called from different folder locations 
+
+clear 
+clc
+path =genpath(cd());
+addpath(path)
+
+% token BioChem
+% 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
+
+
+% printing figures use 
+% handel = gcf
+% print(handle,'-dpdf','figure.pdf')
---
+++ runTheBootStrapOvernight.m
@@ -0,0 +1,4 @@
+% run the two scripts for bootstrap to run over night 
+
+run_parameter_estimation_spike_Vellonella
+run_parameter_estimation_Bacillus
